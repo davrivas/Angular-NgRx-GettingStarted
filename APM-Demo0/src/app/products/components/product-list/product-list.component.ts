8a470b66f009e62f16b5cd745d8afaadbdab4b74
David Rivas <david.rivas@pevaar.com>
2019-11-01 11:24:12 -0500

    Last changes

---
+++ APM-Demo0/src/app/products/components/product-list/product-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'pm-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+    pageTitle = 'Products';
+}
